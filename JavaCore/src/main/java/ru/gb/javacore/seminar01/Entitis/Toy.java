8cfd012d3c57120fda51a20d07e760db7df01090
PavshinSergey <pavshin@mail.ru>
2023-06-13 22:10:39 +0700

    comments added

--- JavaCore/src/main/java/ru/gb/javacore/seminar01/Entitis/Toy.java
+++ JavaCore/src/main/java/ru/gb/javacore/seminar01/Entitis/Toy.java
@@ -1,41 +1,77 @@
 package ru.gb.javacore.seminar01.Entitis;
 
+/**
+ * Class of Toy
+ */
 public class Toy {
     int id;
     String name;
     int count;
     static int number;
 
+    /**
+     * Constructor
+     * @param name - name of Toy
+     * @param count - numbers of instance on stock
+     */
     public Toy(String name, int count) {
         this.name = name;
         this.count = count;
         this.id = getId();
     }
 
+    /**
+     *
+     * @return - returned Id of a Toy
+     */
     private int getId(){
         return ++number;
     }
 
+    /**
+     *
+     * @param id - set id for a Toy
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     *
+     * @return - reterned name of a Toy
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     *
+     * @param name - set name for a Toy
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     *
+     * @return returned numbers of Toy on store
+     */
     public int getCount() {
         return count;
     }
 
+    /**
+     *
+     * @param count - set numbers of a Toy on store
+     */
     public void setCount(int count) {
         this.count = count;
     }
 
+    /**
+     *
+     * @return - to get a string view of current Toy
+     */
     @Override
     public String toString() {
         return "Toy{" +
--- JavaCore/src/main/java/ru/gb/javacore/seminar01/View/Decorator.java
+++ JavaCore/src/main/java/ru/gb/javacore/seminar01/View/Decorator.java
@@ -1,6 +1,10 @@
 package ru.gb.javacore.seminar01.View;
 
 public class Decorator {
+    /**
+     *
+     * @param str - string to print on monitor
+     */
     public static void print(String str){
         System.out.println("Some text put in here: " + str);
     }
